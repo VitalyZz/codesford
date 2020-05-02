942dbffde782ea839e7036d439c4d63875cadc54
Vitaly Garshin <garshin.1999@inbox.ru>
2020-05-03 00:49:39 +0300

    add about us pages

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,7 @@ $('#a li').click(function(){
    var img = $(this).find('img').attr("src");
    var value = $(this).find('img').attr('value') + "233";
    var text = this.innerText;
-   var item = '<li><span class="itemadd">'+ text +'</span><img src="'+ img +'" alt="" /></li>';
+   var item = '<li class="2323"><span class="itemadd">'+ text +'</span><img src="'+ img +'" alt="" /></li>';
   $('.btn-select').html(item);
   $('.btn-select').attr('value', value);
   $(".b").toggle();
@@ -38,7 +38,33 @@ if (sessionLang){
   $('.btn-select').attr('value', sessionLang);
 } else {
    var langIndex = langArray.indexOf('ch');
-  console.log(langIndex);
   $('.btn-select').html(langArray[langIndex]);
   //$('.btn-select').attr('value', 'en');
 }
+let changeElement = document.querySelectorAll('.b #a li');
+let elFirst = changeElement[0].children[0].textContent;
+let elSecond = changeElement[1].children[0].textContent;
+let link = '';
+let link2 = '';
+if (elFirst == 'Ру') {
+  link = document.createElement('a');
+  link.href = 'index.html';
+  link.appendChild(changeElement[0]);
+
+  link2 = document.createElement('a');
+  link2.href = 'main_en.html';
+  link2.appendChild(changeElement[1]);
+}
+else {
+  link = document.createElement('a');
+  link.href = 'main_en.html';
+  link.appendChild(changeElement[0]);
+
+  link2 = document.createElement('a');
+  link2.href = 'index.html';
+  link2.appendChild(changeElement[1]);
+}
+document.querySelector('.b #a').appendChild(link)
+document.querySelector('.b #a').appendChild(link2)
+
+// console.log(document.querySelector('.b #a'));
