a0f3ea4659e7b0ecbd863a1863f3b4a259631fe9
Vitaly Garshin <garshin.1999@inbox.ru>
2020-05-04 01:07:29 +0300

    add new text

--- scripts/script.js
+++ scripts/script.js
@@ -25,9 +25,42 @@ $('#a li').click(function(){
   //console.log(value);
 });
 
-$(".btn-select").click(function(){
-        $(".b").toggle();
-    });
+// $(".btn-select").click(function(){
+//         if ($(".b").css("display") == 'none') {
+//           document.querySelector('.glll').style.transform = 'rotate(-90deg)';
+//         } else {
+//           document.querySelector('.glll').style.transform = 'rotate(90deg)';
+//         }
+//         $(".b").toggle();
+// });
+
+$(".btn-select").click(function() {
+  if ($(".b").css("display") == 'none') {
+    document.querySelector('.glll').style.transform = 'rotate(-90deg)';
+  } else {
+    document.querySelector('.glll').style.transform = 'rotate(90deg)';
+  }
+  $('.b').toggle();
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(".lang-select").length) {
+    $('.b').hide();
+  }
+  if ($(".b").css("display") == 'none') {
+    document.querySelector('.glll').style.transform = 'rotate(90deg)';
+  } else {
+    document.querySelector('.glll').style.transform = 'rotate(-90deg)';
+  }
+  e.stopPropagation();
+});
+
+// $(document).mouseup(function (e) {
+//   var container = $(".b");
+//   if (container.has(e.target).length === 0){
+//       container.hide();
+//   }
+//   document.querySelector('.glll').style.transform = 'rotate(90deg)';
+// });
 
 //check local storage for the lang
 var sessionLang = localStorage.getItem('lang');
@@ -46,25 +79,48 @@ let elFirst = changeElement[0].children[0].textContent;
 let elSecond = changeElement[1].children[0].textContent;
 let link = '';
 let link2 = '';
+let pathName = document.location.pathname;
+console.log(pathName.indexOf("about"));
 if (elFirst == 'Ру') {
   link = document.createElement('a');
-  link.href = 'index.html';
+  if (pathName.indexOf("about") === 1) {
+    link.href = 'about_ru.html';
+  } else {
+    link.href = 'main_ru.html';
+  }
   link.appendChild(changeElement[0]);
 
   link2 = document.createElement('a');
-  link2.href = 'main_en.html';
+  if (pathName.indexOf("about") === 1) {
+    link2.href = 'about_en.html';
+  } else {
+    link2.href = 'index.html';
+  }
   link2.appendChild(changeElement[1]);
 }
 else {
   link = document.createElement('a');
-  link.href = 'main_en.html';
+  if (pathName.indexOf("about")  === 1) {
+    link.href = 'about_en.html';
+  } else {
+    link.href = 'index.html';
+  }
   link.appendChild(changeElement[0]);
 
   link2 = document.createElement('a');
-  link2.href = 'index.html';
+  if (pathName.indexOf("about")  === 1) {
+    link2.href = 'about_ru.html';
+  } else {
+    link2.href = 'main_ru.html';
+  }
   link2.appendChild(changeElement[1]);
 }
 document.querySelector('.b #a').appendChild(link)
 document.querySelector('.b #a').appendChild(link2)
 
-// console.log(document.querySelector('.b #a'));
+// var regexp = /limit=([^&]+)/i;
+//     var GetValue = '';
+//     if (!!regexp.exec(document.location.search)) 
+//         GetValue = regexp.exec(document.location.search)[1];
+$('.btn-select').append('<img src="img/icons/keyboard.svg" alt="youtube" class="glll">')
+console.log(document.location.pathname);
